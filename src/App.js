cb33207af9cea7516d37619ecf60058d8d0f3e77
John Manack <jmnck28@gmail.com>
2021-02-25 10:45:17 -0500

    adds components folder and game and square component files

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import logo from './logo.svg';
+import Game from './components/Game';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <h1>Tic Tac Toe V3</h1>
+      <Game />
     </div>
   );
 }
---
+++ src/components/Game.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class Game extends Component {
+    render() {
+        return (
+            <div className="game">
+                <div className="game-board">
+                    <p>THE BOARD WILL GO HERE</p>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default Game;
